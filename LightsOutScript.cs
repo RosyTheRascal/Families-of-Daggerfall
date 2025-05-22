74306ea9bb89f993583d901bd9ae1d499b126b16
Rosy <lewixiv@gmail.com>
2025-05-22 10:05:07 -0800

    Add files via upload

---
+++ LightsOutScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using DaggerfallConnect;
+using DaggerfallConnect.Arena2;
+using DaggerfallConnect.FallExe;
+using DaggerfallWorkshop;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Serialization;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using DaggerfallWorkshop.Game.Entity;
+using DaggerfallWorkshop.Game.Formulas;
+using DaggerfallWorkshop.Game.Items;
+using DaggerfallWorkshop.Game.UserInterface;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
+using DaggerfallWorkshop.Utility;
+using DaggerfallWorkshop.Game.Utility;
+using DaggerfallWorkshop.Utility.AssetInjection;
+using DaggerfallConnect.Utility;
+
+namespace LightsOutMod
+{
+    public class LightsOut : MonoBehaviour
+    {
+        private static Mod mod;
+
+        [Invoke(StateManager.StateTypes.Start, 0)]
+        public static void Init(InitParams initParams)
+        {
+            mod = initParams.Mod;
+
+            var go = new GameObject(mod.Title);
+            go.AddComponent<LightsOut>();
+
+            mod.IsReady = true;
+        }
+
+        private void Start()
+        {
+            
+        }
+    }
+}
